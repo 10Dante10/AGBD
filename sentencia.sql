b54efe06ec31a9d7c5f85d4a5ba44412cc23777c
DanteFama <danttee.fama@gmail.com>
2024-03-21 12:09:42 -0300

    snetencia juegosql island

---
+++ sentencia.sql
@@ -0,0 +1,7 @@
+SELECT * FROM INHABITANT
+where state="friendly"
+and job="weaponsmith"
+where job like "%smith" and state="friendly"
+select personid from inhabitant where name="Stranger"
+select gold from inhabitant where name="Stranger"
+select * from item WHERE owner is null
